04aab26ff348534054f7a9fbcb1ecbe6b311b66e
Shady Boukhary <sb199898.sb@gmail.com>
2019-01-08 21:05:00 +0200

    started function detection

--- src/data/grader/source_analyzer.py
+++ src/data/grader/source_analyzer.py
@@ -13,12 +13,30 @@ def analyze_source(path, assignment, individual_assignment):
         if result:
             source = Utilities.read_file(path)
             # TODO: Keep track of functions in source, maybe loops too?
+            analyze(source, "cpp")
             Utilities.log(Constants.CHECK_MARK)
         else:
             raise CompilationException(Constants.CROSS_MARK, error)
     except IOError as e:
         Utilities.log(e)
 
+
+def analyze(source, language):
+    source = clean_source(source)
+    print(source)
+
+    if language == "cpp":
+        analyze_cpp(source)
+
+
+def analyze_cpp(source):
+    lines_with_types = [line for line in source if Constants.CPP_PRIMITIVE_TYPES.__contains__(line.split()[0])]
+    print(lines_with_types)
+
+
+
+def clean_source(source):
+    return Utilities.correct_input(source).splitlines()
 
 def verify_compilation(path, compile_output_dir, compile_output_path, language):
     """Checks whether a source code for an assignment compiles
--- src/utils/constants.py
+++ src/utils/constants.py
@@ -13,3 +13,8 @@ class Constants:
     CLONE_DIRECTORY = "/automated_grading_repos"
     CROSS_MARK = "\u274c"
     CHECK_MARK = "\u2713"
+    CPP_PRIMITIVE_TYPES = [
+        "short int", "unsigned short int", "unsigned int", "int",
+        "long int", "unsigned long int", "long long int", "unsigned long long int",
+        "signed char", "char", "unsigned char", "float", "double", "long double", "wchar_t,", "void"
+    ]
